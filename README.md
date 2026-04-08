417edb9cc7fe75b51ce866d8daa5fe5fa2a026e9
Pournima <pournimasjathade@gmail.com>
2026-04-08 16:11:25 -0700

    Fixed API integration and backend connection

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ Create a .env file in housing-backend:
 PORT=3000
 DB_HOST=localhost
 DB_USER=root
-DB_PASSWORD=Develop@123
+DB_PASSWORD=************
 DB_NAME=housing_app
 
 
--- housing-frontend/src/app/routes.ts
+++ housing-frontend/src/app/routes.ts
@@ -16,12 +16,3 @@ const routeConfig: Routes = [
 ];
 
 export default routeConfig;
-// import { Routes } from '@angular/router';
-// import { HomeComponent } from './home/home.component';
-// import { DetailsComponent } from './details/details.component';
-
-// export const routes: Routes = [
-//   { path: '', component: HomeComponent },
-//   { path: 'details/:id', component: DetailsComponent }
-// ];
-// export default routes;
